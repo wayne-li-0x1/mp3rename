588a63a3b3414c1aee7326c70d7d73dcc1db7f7d
Wayne Li <wayne-li-0x1@gmail.com>
2019-08-22 20:39:43 -0700

    add example mp3 and debug config

--- mp3rename.py
+++ mp3rename.py
@@ -3,3 +3,13 @@
 #pylint: disable=W0141
 
 import argparse
+import sys
+import os
+
+#Dependencies:
+#pip3 install pypinyin
+#https://github.com/mozillazg/python-pinyin
+import pypinyin 
+
+print(sys.argv)
+
